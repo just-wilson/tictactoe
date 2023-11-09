a3a0340c20fe7cd9c2199af04a54354ff06c1dfd
Justin Wilson <justin.wilson@neudesic.com>
2023-11-09 12:32:31 -0500

    build board

--- tic-tac-toe/src/components/Board.tsx
+++ tic-tac-toe/src/components/Board.tsx
@@ -2,5 +2,31 @@ import Square from "./Square";
 
 type boardProps = {
     squares: string[];
-    onClick: (i: number) => void;
+    isNextX: boolean;
+};
+
+const Board = ({squares, isNextX}: boardProps) => {
+    const assignSquare = (index: number) => {
+        if  (squares[index] !== null) {
+            return;
+        } else if (isNextX) {
+            squares[index] = "X";
+        } else {
+            squares[index] = "O";
+        }
+    };
+    // do some logic here
+
+    return (
+        <>
+        {[0, 1, 2].map((rowIndex) => {
+            <div className="board-row">
+                {[0, 1, 2].map((colIndex) => {
+                    const index = rowIndex * 3 + colIndex;
+                    return <Square value={squares[index]} onSquareClick={() => {assignSquare(index)}} />
+                })}
+            </div>
+        })}
+        </>
+    );
 };
